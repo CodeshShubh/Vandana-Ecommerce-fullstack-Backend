403eec5f1db836415bcf7debb4b3ca56de9da1e1
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-05-22 13:04:49 +0530

    uuid package( for if same photo upload then it create diffent name in upload folder), app.ts(productRoutes+express.static- to upload files in local mechinecode folder), product.ts(newProduct creation), Multer.ts( used of uuid),product.ts(schema), product.ts(product routees wwith singleUpload and adminOnly), types.ts(newProductRequestBody types) , image which we upload during api hit

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import { config } from "dotenv";
 
 // Importing Routes
 import userRoute from "./routes/user.js";
-// import productRoute from "./routes/products.js";
+import productRoute from "./routes/products.js";
 // import orderRoute from "./routes/order.js";
 // import paymentRoute from "./routes/payment.js";
 // import dashboardRoute from "./routes/stats.js";
@@ -40,12 +40,12 @@ app.get("/", (req, res) => {
 
 // Using Routes
 app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/product", productRoute);
+app.use("/api/v1/product", productRoute);
 // app.use("/api/v1/order", orderRoute);
 // app.use("/api/v1/payment", paymentRoute);
 // app.use("/api/v1/dashboard", dashboardRoute);
 
-// app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads")); // to use upload photo by product/new in frontend
 app.use(errorMiddleware);
 
 app.listen(port, () => {
--- src/types/types.ts
+++ src/types/types.ts
@@ -9,12 +9,12 @@ export interface NewUserRequestBody {
   dob: Date;
 }
 
-// export interface NewProductRequestBody {
-//   name: string;
-//   category: string;
-//   price: number;
-//   stock: number;
-// }
+export interface NewProductRequestBody {
+  name: string;
+  category: string;
+  price: number;
+  stock: number;
+}
 
 export type ControllerType = (
   req: Request,
